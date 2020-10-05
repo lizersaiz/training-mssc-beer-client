4a1db8f19a5a754bcdbc67ef6f8f162f118dc708
lizersaiz <lizer.saiz.ads@gmail.com>
2020-10-05 18:26:25 +0200

    http-post

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,9 +18,32 @@ class BreweryClientTest {
 
     @Test
     void getBeerById() {
+    	
         BeerDto dto = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(dto);
-
+    }
+    
+    @Test
+    void saveNewBeer() {
+    	
+    	//given
+    	BeerDto dto = BeerDto.builder().id(null)
+    			.beerName("create beer1")
+    			.build();
+    	URI uri = client.saveNewBeer(dto);
+    	
+    	assertNotNull(uri);
+    }
+    
+    @Test
+    void updateBeer() {
+    	
+    	//given
+    	BeerDto dto = BeerDto.builder().id(null)
+    			.beerName("update beer")
+    			.build();
+    	
+    	client.updateBeer(UUID.randomUUID(), dto);
     }
 }
