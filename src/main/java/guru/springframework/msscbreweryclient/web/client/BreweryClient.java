ba5d058805e5458d07268f9754816a26682cb1ef
lizersaiz <lizer.saiz.ads@gmail.com>
2020-10-05 18:46:19 +0200

    http-post

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 /**
@@ -28,6 +29,17 @@ public class BreweryClient {
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
     }
 
+    public URI saveNewBeer(BeerDto beerDto) {
+    	
+    	return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+    
+    public void updateBeer(UUID uuid, BeerDto beerDto) {
+    	
+    	restTemplate.put(apihost + BEER_PATH_V1 + uuid, beerDto);
+    }
+    
+    
     public void setApihost(String apihost) {
         this.apihost = apihost;
     }
